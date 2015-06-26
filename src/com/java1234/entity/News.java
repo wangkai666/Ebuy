fb2f50dc9044b18451d12584570d784f0bf11048
wangkai666 <15268561861@163.com>
2015-06-27 01:20:39 +0800

    EBuy v1.0 基本框架的搭建

---
+++ src/com/java1234/entity/News.java
@@ -0,0 +1,85 @@
+package com.java1234.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * 新闻
+ *
+ * @author wangkai
+ *
+ */
+@Entity
+@Table(name="t_news")
+public class News {
+
+	/**
+	 * 主键，PK
+	 */
+	private int id;
+	
+	/**
+	 * 标题
+	 */
+	private String title;
+	
+	/**
+	 * 内容
+	 */
+	private String content;
+	
+	/**
+	 * 创建时间
+	 */
+	private Date createName;
+
+	@Id
+	@GeneratedValue(generator="_native")
+	@GenericGenerator(name="_native",strategy="native")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(length=50)
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Lob
+	@Column(columnDefinition="TEXT")
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getCreateName() {
+		return createName;
+	}
+
+	public void setCreateName(Date createName) {
+		this.createName = createName;
+	}
+	
+	
+	
+	
+}
