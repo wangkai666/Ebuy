4df5ef2d1e45fff792576d2d2d9ac1a5293a2beb
wangkai666 <15268561861@163.com>
2015-06-28 11:07:10 +0800

    商品大类和商品小类

---
+++ src/com/java1234/entity/ProductSmallType.java
@@ -0,0 +1,63 @@
+package com.java1234.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * 商品小类
+ *
+ * @author wangkai
+ *
+ */
+@Entity
+@Table(name="t_smallType")
+public class ProductSmallType {
+
+	/**
+	 * 主键，pk
+	 */
+	private int id;
+	
+	/**
+	 * 名称
+	 */
+	private String name;
+	
+	/**
+	 * 备注
+	 */
+	private String memo;
+
+	@Id
+	@GeneratedValue(generator="_native")
+	@GenericGenerator(name="_native",strategy="native")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(length=50)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+}
